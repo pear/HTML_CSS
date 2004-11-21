36e5caeb316d91eb5e3fe00c2174dc8d96e2af4f
Laurent Laville <farell@php.net>
2004-11-21 18:56:10 +0000

    add new testcase for api setXhtmlCompliance missing in previous versions
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_CSS/trunk@173153 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ tests/HTML_CSS_TestCase_setXhtmlCompliance.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * API Unit tests for HTML_CSS package.
+ * 
+ * @version    $Id$
+ * @author     Laurent Laville <pear@laurent-laville.org>
+ * @package    HTML_CSS
+ */
+
+require_once 'PEAR.php';
+
+class HTML_CSS_TestCase_setXhtmlCompliance extends PHPUnit_TestCase
+{
+    /**
+     * A CSS object
+     * @var        object
+     */
+    var $stylesheet;
+
+    function HTML_CSS_TestCase_setXhtmlCompliance($name)
+    {
+        $this->PHPUnit_TestCase($name);
+    }
+
+    function setUp()
+    {
+        error_reporting(E_ALL & ~E_NOTICE);
+
+        $logger['pushCallback'] = array(&$this, '_pushCallback'); // don't die when an exception is thrown
+        $attrs = array();
+        $this->stylesheet = new HTML_CSS($attrs, $logger);
+    }
+
+    function tearDown()
+    {
+        unset($this->stylesheet);
+    }
+
+    function _stripWhitespace($str)
+    {
+        return preg_replace('/\\s+/', '', $str);
+    }
+
+    function _methodExists($name) 
+    {
+        if (substr(PHP_VERSION,0,1) < '5') {
+            $n = strtolower($name);
+        } else {
+            $n = $name;
+        }
+        if (in_array($n, get_class_methods($this->stylesheet))) {
+            return true;
+        }
+        $this->assertTrue(false, 'method '. $name . ' not implemented in ' . get_class($this->stylesheet));
+        return false;
+    }
+
+    function _pushCallback($code, $level)
+    {
+        // don't die if the error is an exception (as default callback)
+        return true;
+    }
+
+    function _getResult($res)
+    {
+        if (PEAR::isError($res)) {
+            $this->assertTrue(false, $res->getMessage());
+        } else {
+            $this->assertTrue(true);
+	}
+    }
+
+
+    /**
+     * Tests a setXhtmlCompliance method 
+     *
+     * - fail1: wrong group id
+     */  
+    function test_setXhtmlCompliance_fail()
+    {
+        $c = $this->stylesheet->setXhtmlCompliance('true');
+        $this->_getResult($c);
+    }
+
+    function test_setXhtmlCompliance()
+    {
+        $c = $this->stylesheet->setXhtmlCompliance(true);
+        $this->_getResult($c);
+    }
+}
+?>
