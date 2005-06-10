611ce4008e3c10e2333a5ec3b0446f74770d8c1b
Laurent Laville <farell@php.net>
2005-06-10 16:26:02 +0000

    - Updates headers comment block - Use HTML_Page2 package instead of HTML_Page (deprecated)
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_CSS/trunk@188108 c90b9560-bf6c-de11-be94-00142212c4b1

--- examples/CSS_InHeader.php
+++ examples/CSS_InHeader.php
@@ -1,24 +1,28 @@
 <?php
-/* vim: set expandtab tabstop=4 shiftwidth=4: */
-// +----------------------------------------------------------------------+
-// | PHP Version 4                                                        |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 1997 - 2004 The PHP Group                              |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 3.0 of the PHP license,       |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available at through the world-wide-web at                           |
-// | http://www.php.net/license/3_0.txt.                                  |
-// | If you did not receive a copy of the PHP license and are unable to   |
-// | obtain it through the world-wide-web, please send a note to          |
-// | license@php.net so we can mail you a copy immediately.               |
-// +----------------------------------------------------------------------+
-// | Author:  Klaus Guenther <klaus@capitalfocus.org>                     |
-// +----------------------------------------------------------------------+
-//
-// $Id$
+/**
+ * InHeader example
+ *
+ *
+ * PHP versions 4 and 5
+ *
+ * LICENSE: This source file is subject to version 3.0 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category   HTML
+ * @package    HTML_CSS
+ * @subpackage Examples
+ * @author     Klaus Guenther <klaus@capitalfocus.org>
+ * @author     Laurent Laville <pear@laurent-laville.org>
+ * @copyright  1997-2005 The PHP Group
+ * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @version    CVS: $Id$
+ * @link       http://pear.php.net/package/HTML_CSS
+ */
 
-require_once 'HTML/Page.php';
+require_once 'HTML/Page2.php';
 require_once 'HTML/CSS.php';
 
 // generate an instance
@@ -33,16 +37,16 @@ $css->setStyle('h1', 'font', '16pt helvetica, arial, sans-serif');
 // define <p> element
 $css->setStyle('p', 'font', '12pt helvetica, arial, sans-serif');
 
-// create a new HTML page
-$p = new HTML_Page();
+// create a new HTML page2 instance
+$p = new HTML_Page2();
 $p->setTitle("My page");
 // it can be added as an object
 $p->addStyleDeclaration($css, 'text/css');
-$p->setMetaData("author", "My Name");
-$p->addBodyContent("<h1>headline</h1>");
-$p->addBodyContent("<p>some text</p>");
-$p->addBodyContent("<p>some more text</p>");
-$p->addBodyContent("<p>yet even more text</p>");
+$p->setMetaData('author', 'My Name');
+$p->addBodyContent('<h1>headline</h1>');
+$p->addBodyContent('<p>some text</p>');
+$p->addBodyContent('<p>some more text</p>');
+$p->addBodyContent('<p>yet even more text</p>');
 // output the finished product to the browser
 $p->display();
 // or output the finished product to a file
