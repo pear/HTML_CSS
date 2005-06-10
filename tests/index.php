3083e2f4fa50cf97ec1ca95e366228b9b0331118
Laurent Laville <farell@php.net>
2005-06-10 17:17:24 +0000

    removes TestCase marked as deprecated
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_CSS/trunk@188115 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/index.php
+++ tests/index.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * HTML output for PHPUnit suite tests.
- * 
+ *
  * @version    $Id$
  * @author     Laurent Laville <pear@laurent-laville.org>
  * @package    HTML_CSS
@@ -21,12 +21,12 @@ $title = 'PhpUnit test run, HTML_CSS class';
 <body>
 <h1><?php echo $title; ?></h1>
       <p>
-	This page runs all the phpUnit self-tests, and produces nice HTML output.
+    This page runs all the phpUnit self-tests, and produces nice HTML output.
       </p>
       <p>
-	Unlike typical test run, <strong>expect many test cases to
-	  fail</strong>.  Exactly those with <code>pass</code> in their name
-	should succeed.
+    Unlike typical test run, <strong>expect many test cases to
+      fail</strong>.  Exactly those with <code>pass</code> in their name
+    should succeed.
       </p>
       <p>
       For each test we display both the test result -- <span
@@ -40,19 +40,16 @@ $title = 'PhpUnit test run, HTML_CSS class';
       for a few 'unknown' meta-results (because of errors) when running
       in PHP3.
       </p>
-      
+
 <h2>Tests</h2>
-	<?php
-	$testcases = array(
+    <?php
+    $testcases = array(
             'HTML_CSS_TestCase_setSingleLineOutput',
-            'HTML_CSS_TestCase_parseSelectors',
             'HTML_CSS_TestCase_setXhtmlCompliance',
             'HTML_CSS_TestCase_createGroup',
             'HTML_CSS_TestCase_unsetGroup',
             'HTML_CSS_TestCase_setGroupStyle',
             'HTML_CSS_TestCase_getGroupStyle',
-            'HTML_CSS_TestCase_addGroupSelector',
-            'HTML_CSS_TestCase_removeGroupSelector',
             'HTML_CSS_TestCase_setStyle',
             'HTML_CSS_TestCase_getStyle',
             'HTML_CSS_TestCase_setSameStyle',
@@ -67,19 +64,19 @@ $title = 'PhpUnit test run, HTML_CSS class';
             'HTML_CSS_TestCase_bug1066',
             'HTML_CSS_TestCase_bug1072',
             'HTML_CSS_TestCase_bug1084'
-	);
-	
-	$suite = new PHPUnit_TestSuite();
+    );
 
-	foreach ($testcases as $testcase) {
-    	    include_once $testcase . '.php';
+    $suite = new PHPUnit_TestSuite();
+
+    foreach ($testcases as $testcase) {
+            include_once $testcase . '.php';
             $suite->addTestSuite($testcase);
-	}
+    }
 
-	$listener = new HTML_TestListener();
+    $listener = new HTML_TestListener();
         $result = TestUnit::run($suite, $listener);
-	$result->removeListener($listener);
-	$result->report();
-	?>
+    $result->removeListener($listener);
+    $result->report();
+    ?>
 </body>
 </html>
