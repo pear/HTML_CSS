784688e1a1847735c8cdf5b9c1b4a7efd51d0e64
Laurent Laville <farell@php.net>
2006-06-17 14:27:49 +0000

    changed copyright dates (2003 is first release of HTML_CSS) and minor fixes in header comment blocks
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_CSS/trunk@214966 c90b9560-bf6c-de11-be94-00142212c4b1

--- CSS.php
+++ CSS.php
@@ -2,9 +2,6 @@
 /**
  * Base class for CSS definitions
  *
- * This class handles the details for creating properly
- * constructed CSS declarations.
- *
  * PHP versions 4 and 5
  *
  * LICENSE: This source file is subject to version 3.0 of the PHP license
@@ -17,7 +14,7 @@
  * @package    HTML_CSS
  * @author     Klaus Guenther <klaus@capitalfocus.org>
  * @author     Laurent Laville <pear@laurent-laville.org>
- * @copyright  1997-2005 The PHP Group
+ * @copyright  2003-2006 The PHP Group
  * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
  * @version    CVS: $Id$
  * @link       http://pear.php.net/package/HTML_CSS
@@ -47,19 +44,11 @@ define ('HTML_CSS_ERROR_WRITE_FILE',            -106);
  * This class handles the details for creating properly
  * constructed CSS declarations.
  *
- * PHP versions 4 and 5
- *
- * LICENSE: This source file is subject to version 3.0 of the PHP license
- * that is available through the world-wide-web at the following URI:
- * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
- * the PHP License and are unable to obtain it through the web, please
- * send a note to license@php.net so we can mail you a copy immediately.
- *
  * @category   HTML
  * @package    HTML_CSS
  * @author     Klaus Guenther <klaus@capitalfocus.org>
  * @author     Laurent Laville <pear@laurent-laville.org>
- * @copyright  1997-2005 The PHP Group
+ * @copyright  2003-2006 The PHP Group
  * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
  * @version    Release: @package_version@
  * @link       http://pear.php.net/package/HTML_CSS
