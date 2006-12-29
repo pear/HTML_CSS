0de4b2554fc1eb74e1581d277727fa2603d6b5c6
Laurent Laville <farell@php.net>
2006-12-29 12:47:32 +0000

    upgrade license and copyright notice for incoming new stable release 1.1.0
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_CSS/trunk@225995 c90b9560-bf6c-de11-be94-00142212c4b1

--- examples/CSS_parseData.php
+++ examples/CSS_parseData.php
@@ -5,9 +5,9 @@
  *
  * PHP versions 4 and 5
  *
- * LICENSE: This source file is subject to version 3.0 of the PHP license
+ * LICENSE: This source file is subject to version 3.01 of the PHP license
  * that is available through the world-wide-web at the following URI:
- * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
+ * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
  * the PHP License and are unable to obtain it through the web, please
  * send a note to license@php.net so we can mail you a copy immediately.
  *
@@ -15,8 +15,8 @@
  * @package    HTML_CSS
  * @subpackage Examples
  * @author     Laurent Laville <pear@laurent-laville.org>
- * @copyright  2005-2006 The PHP Group
- * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @copyright  2005-2007 The PHP Group
+ * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
  * @version    CVS: $Id$
  * @link       http://pear.php.net/package/HTML_CSS
  * @since      File available since Release 1.0.0RC2
