0bf64be1b6317b5404636b9cab38404d0fcc761b
Laurent Laville <farell@php.net>
2007-01-01 14:10:38 +0000

    - fixed version number on license declared and used - isError() did not use any argument
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_CSS/trunk@226183 c90b9560-bf6c-de11-be94-00142212c4b1

--- CSS.php
+++ CSS.php
@@ -4,9 +4,9 @@
  *
  * PHP versions 4 and 5
  *
- * LICENSE: This source file is subject to version 3.0 of the PHP license
+ * LICENSE: This source file is subject to version 3.01 of the PHP license
  * that is available through the world-wide-web at the following URI:
- * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
+ * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
  * the PHP License and are unable to obtain it through the web, please
  * send a note to license@php.net so we can mail you a copy immediately.
  *
@@ -1265,7 +1265,7 @@ class HTML_CSS extends HTML_Common
             } else {
                 $res = $this->parseString($style, $duplicates);
             }
-            if ($this->isError($res)) {
+            if ($this->isError()) {
                 return $res;
             }
         }
