af32708d6afa6e35ce2684d7d33857fd2771cbf3
Laurent Laville <farell@php.net>
2007-12-04 10:37:08 +0000

    initial commit : - migration from PHPUnit test suite 1.x to 3.x
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_CSS/trunk@247576 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ tests/AllTests.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Test suite for HTML_CSS
+ *
+ * PHP version 5
+ *
+ * @category HTML
+ * @package  HTML_CSS
+ * @author   Laurent Laville <pear@laurent-laville.org>
+ * @license  http://www.opensource.org/licenses/bsd-license.php BSD
+ * @version  CVS: $Id$
+ * @link     http://pear.php.net/package/HTML_CSS
+ * @since    File available since Release 1.4.0
+ */
+
+if (!defined('PHPUnit_MAIN_METHOD')) {
+    define('PHPUnit_MAIN_METHOD', 'HTML_CSS_AllTests::main');
+}
+
+require_once 'PHPUnit/Framework/TestSuite.php';
+require_once 'PHPUnit/TextUI/TestRunner.php';
+
+chdir(dirname(__FILE__));
+
+require_once 'HTML_CSS_TestSuite_Standard.php';
+
+/**
+ * Class for running all test suites for HTML_CSS package.
+ *
+ * @category HTML
+ * @package  HTML_CSS
+ * @author   Laurent Laville <pear@laurent-laville.org>
+ * @license  http://www.opensource.org/licenses/bsd-license.php BSD
+ * @version  Release: $id$
+ * @link     http://pear.php.net/package/HTML_CSS
+ * @since    File available since Release 1.4.0
+ */
+
+class HTML_CSS_AllTests
+{
+    /**
+     * Runs the test suite.
+     *
+     * @return void
+     * @static
+     */
+    public static function main()
+    {
+        PHPUnit_TextUI_TestRunner::run(self::suite());
+    }
+
+    /**
+     * Adds the HTML_CSS test suite.
+     *
+     * @return object the PHPUnit_Framework_TestSuite object
+     * @static
+     */
+    public static function suite()
+    {
+        $suite = new PHPUnit_Framework_TestSuite('HTML_CSS Test Suite');
+        $suite->addTestSuite('HTML_CSS_TestSuite_Standard');
+        return $suite;
+    }
+}
+
+if (PHPUnit_MAIN_METHOD == 'HTML_CSS_AllTests::main') {
+    HTML_CSS_AllTests::main();
+}
+?>
