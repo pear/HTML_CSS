2501b4147478e4022615860be540e02b9f4ffa7c
Laurent Laville <farell@php.net>
2007-12-10 10:55:03 +0000

    package script generator ready for stable release 1.4.0
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_CSS/trunk@247885 c90b9560-bf6c-de11-be94-00142212c4b1

--- package.php
+++ package.php
@@ -30,26 +30,35 @@ $options = array('filelistgenerator' => 'cvs',
     'simpleoutput' => true,
     'clearcontents' => false,
     'changelogoldtonew' => false,
-    'ignore' => array('CSS_Advanced.php', 'Advanced.php', 'package.php')
+    'ignore' => array(__FILE__)
     );
 
 $p2 = &PEAR_PackageFileManager2::importOptions($packagefile, $options);
 $p2->setPackageType('php');
 $p2->addRelease();
 $p2->generateContents();
-$p2->setReleaseVersion('1.3.0');
-$p2->setAPIVersion('1.3.0');
+$p2->setReleaseVersion('1.4.0');
+$p2->setAPIVersion('1.4.0');
 $p2->setReleaseStability('stable');
 $p2->setAPIStability('stable');
 $p2->setNotes('* changes
-- prevent invalide CSS data source in to parse
+- removed old class (private) properties related to options ($_xhtmlCompliant,
+  $_cache, $_singleLine, $_charset, $_contentDisposition, $_groupsFirst,
+  $_allowDuplicates), now group by in a private array $options.
+- added class constructor (ZE2) for PHP5.
+- Error handler allow now to use PEAR_ERROR_CALLBACK to customize action
+  (log yes/no, print yes/no) when an error/exception is raised.
+- remove trailing EOL in toString() output (with oneline option set to true)
 
 * news
-- add Content-Disposition to the headers in display(); implement request #12195
+- API 1.4.0 allow now a setter/getter PHP5 facility compatible
+  (magic function __set, __get) for read/write CSS options.
 
 * QA
-User Guide 1.3.0 included in this release cover all versions
-1.x.x, 1.1.x, 1.2.x, 1.3.x
+- Coding Standard fixes (recommandation by PHP_CodeSniffer)
+- tests suite migrated from PHPUnit 1.x to 3.x
+- User Guide 1.4.0 included in this release cover all versions
+1.x.x, 1.1.x, 1.2.x, 1.3.x, 1.4.x
 ');
 $p2->setLicense('BSD', 'http://www.opensource.org/licenses/bsd-license.php');
 
